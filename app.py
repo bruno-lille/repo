94ce1559686e7f1705dcb24912f4a7ea8f4f6c14
Bruno Legrand <bruno-lille@live.com>
2026-05-05 12:33:53 +0200

    auto-save before pull 2026-05-05_12-33-53

--- app.py
+++ app.py
@@ -622,7 +622,7 @@ nav_buttons = """
 app = Flask(__name__)
 
 APP_VERSION = "V1-dev"
-APP_BUILD = "2026-05-05_12-01-44"
+APP_BUILD = "2026-05-05_12-27-58"
 APP_NOTE = "dev en cours"
 
 
@@ -841,8 +841,8 @@ def home():
 
         bloc += f"""
         <div class="card">
-            href="/suggest_update/NEW?q={query_encoded}"
-                🔎 Plus de résultats TMDB
+            <a class="btn update" href="/suggest_update/NEW?q={query_encoded}">
+                ✏️ Autre Recherche/Ajout
             </a>
         </div>
         """
